109e32f1fac4798e67db81e3a93b91dd77328211
Panos Athanasiou <panos_ath@outlook.com>
2019-09-16 14:29:15 +0300

    Add ObservableObject and ViewModelBase

---
+++ src/OpenExtensions/MVVM/ObservableObject.shared.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OpenExtensions.MVVM
+{
+    /// <summary>
+    /// Object that implements the <see cref="INotifyPropertyChanged"/> interface.
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Raised when a property is changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets the new value if its different from the old and raises a property changed event.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="newValue"></param>
+        /// <param name="propertyName"></param>
+        /// <returns>True if the value was set.</returns>
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
