3cb29ca97049cbb20f8775f06ec27e42509b5779
Panos Athanasiou <panos_ath@outlook.com>
2019-09-16 14:20:58 +0300

    Move core extensions to its own folder.
    Make the class partial and create different cs files for different extensions.

---
+++ src/OpenExtensions/Core/Extensions/CollectionExtensions.shared.cs
@@ -0,0 +1,39 @@
+﻿using OpenExtensions.Core.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace OpenExtensions.Core
+{
+    /// <summary>
+    /// Extension methods.
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Return any <see cref="IEnumerable{T}"/> to an <see cref="OCollection{T}"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static OCollection<T> ToOcollection<T>(this IEnumerable<T> collection)
+        {
+            return new OCollection<T>(collection);
+        }
+
+        /// <summary>
+        /// A for each extension tha can be called in a fluent manner.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> FluentForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+            return collection;
+        }
+    }
+}
--- src/OpenExtensions/Core/Extensions/EventExtensions.shared.cs
+++ src/OpenExtensions/Core/Extensions/EventExtensions.shared.cs
@@ -1,42 +1,10 @@
-﻿using OpenExtensions.Core.Collections;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
 
 namespace OpenExtensions.Core
 {
-    /// <summary>
-    /// Extension methods.
-    /// </summary>
-    public static class Extensions
+    public static partial class Extensions
     {
-        /// <summary>
-        /// Return any <see cref="IEnumerable{T}"/> to an <see cref="OCollection{T}"/>
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <returns></returns>
-        public static OCollection<T> ToOcollection<T>(this IEnumerable<T> collection)
-        {
-            return new OCollection<T>(collection);
-        }
-
-        /// <summary>
-        /// A for each extension tha can be called in a fluent manner.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="action"></param>
-        /// <returns></returns>
-        public static IEnumerable<T> FluentForEach<T>(this IEnumerable<T> collection, Action<T> action)
-        {
-            foreach (var item in collection)
-            {
-                action(item);
-            }
-            return collection;
-        }
-
         /// <summary>
         /// Invokes the handlers attached to an eventhandler.
         /// </summary>
@@ -44,7 +12,7 @@ namespace OpenExtensions.Core
         /// <param name="eventHandler">The EventHandler</param>
         /// <param name="sender"></param>
         /// <param name="args"></param>
-        public static void RaiseEvent<T>(EventHandler<T> eventHandler, object sender, T args)
+        public static void RaiseEvent<T>(this EventHandler<T> eventHandler, object sender, T args)
         {
             EventHandler<T> handler = eventHandler;
 
@@ -66,7 +34,7 @@ namespace OpenExtensions.Core
         /// <param name="eventHandler">The EventHandler</param>
         /// <param name="sender"></param>
         /// <param name="args"></param>
-        public static void RaiseCancelableEvent<T>(EventHandler<T> eventHandler, object sender, T args) where T : CancelEventArgs
+        public static void RaiseCancelableEvent<T>(this EventHandler<T> eventHandler, object sender, T args) where T : CancelEventArgs
         {
             EventHandler<T> handler = eventHandler;
 
