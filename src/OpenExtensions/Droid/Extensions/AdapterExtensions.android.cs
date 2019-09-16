053a67ee8e1b933acd10c0f2288d2369618dcdc4
Panos Athanasiou <panos_ath@outlook.com>
2019-09-16 14:21:34 +0300

    Move extensions to their own folder.
    Make the class partial an create cs files for a group of extensions

---
+++ src/OpenExtensions/Droid/Extensions/AdapterExtensions.android.cs
@@ -0,0 +1,47 @@
+﻿using Android.Content;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenExtensions.Droid
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// An extension to turn a string collection into an <see cref="ArrayAdapter{T}"/> adapter of string.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="context"></param>
+        /// <param name="textViewResourceId">Leave 0 to use the default <see cref="Resource.Layout.support_simple_spinner_dropdown_item"/></param>
+        /// <returns></returns>
+        public static ArrayAdapter<string> ToArrayAdapter(
+            this IEnumerable<string> collection,
+            Context context,
+            int textViewResourceId = 0)
+        {
+            if (textViewResourceId == 0)
+            {
+                return new ArrayAdapter<string>(context, Resource.Layout.support_simple_spinner_dropdown_item, collection.ToList());
+            }
+            return new ArrayAdapter<string>(context, textViewResourceId, collection.ToList());
+        }
+
+        /// <summary>
+        /// An extension to turn a collection of T into an <see cref="ArrayAdapter{T}"/> adapter of string.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="context"></param>
+        /// <param name="selector">A fucn to choose the string title of the item.</param>
+        /// <param name="textViewResourceId">Leave 0 to use the default <see cref="Resource.Layout.support_simple_spinner_dropdown_item"/></param>
+        /// <returns></returns>
+        public static ArrayAdapter<string> ToArrayAdapter<T>(
+            this IEnumerable<T> collection,
+            Context context,
+            Func<T, string> selector,
+            int textViewResourceId = 0)
+        {
+            return ToArrayAdapter(collection.Select(selector), context, textViewResourceId);
+        }
+    }
+}
---
+++ src/OpenExtensions/Droid/Extensions/CursorExtensions.android.cs
@@ -0,0 +1,55 @@
+﻿using Android.Content;
+using Android.Database;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace OpenExtensions.Droid
+{
+    /// <summary>
+    /// Extension methods for anything that has to do with android and mvvmlight.
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Returns a SimpleCursorAdapter with a MatrixCursor as the cursor.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="layout">the layout to use to display the properties.</param>
+        /// <param name="to">id of the view for the displayed property.</param>
+        public static SimpleCursorAdapter CreateSimpleCursorAdapter(Context context, int layout, int[] to)
+        {
+            return new SimpleCursorAdapter(context, layout, null, new string[] { "text" }, to, CursorAdapterFlags.None);
+        }
+
+        /// <summary>
+        /// Returns a SimpleCursorAdapter with a MatrixCursor as the cursor.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="layout">the layout to use to display the properties.</param>
+        /// <param name="to">id of the view for the displayed property.</param>
+        public static Android.Support.V4.Widget.SimpleCursorAdapter CreateSimpleCursorAdapterSupport(Context context, int layout, int[] to)
+        {
+            return new Android.Support.V4.Widget.SimpleCursorAdapter(context, layout, null, new string[] { "text" }, to);
+        }
+
+        /// <summary>
+        /// Returns a cursor adapter, use <see cref="CreateSimpleCursorAdapter(Context, int, int[])"/> to create an adapter.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="text">The text to display</param>
+        /// <param name="serialize">A way to serialize the object and retrieve it from the third column => [2] as a string.</param>
+        public static MatrixCursor ToMatrixCursor<T>(this IEnumerable<T> collection, Func<T, string> text, Func<T, string> serialize = null)
+        {
+            MatrixCursor cursor = new MatrixCursor(new string[] { "_id", "text", "item" });
+
+            int id = 0;
+            foreach (var item in collection)
+            {
+                cursor.AddRow(new Java.Lang.Object[] { id.ToString(), text?.Invoke(item), serialize?.Invoke(item) });
+                id++;
+            }
+            return cursor;
+        }
+    }
+}
